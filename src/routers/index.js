d97aba3c1dc3aba38daed9931333d2a8d589463a
Jorge Trujillo <88351609+MatiusRock1@users.noreply.github.com>
2021-08-18 16:44:11 -0500

    mutter s3

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,18 +1,42 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
-const mimeTypes = require('mime-types')
+const mimeTypes = require('mime-types');
+var aws = require('aws-sdk');
+var multerS3 = require('multer-s3');
 
-const storage = multer.diskStorage({
-  destination : 'uploads/',
-  filename : function(req,file,cb){
-      cb("",Date.now() + "." + mimeTypes.extension(file.mimetype));
-  }
-});
+aws.config.update({region: 'us-west-2'});
+var s3 = new aws.S3({apiVersion: '2006-03-01'})
 
-const upload = multer({
-    storage : storage
-})
+
+
+const uploads3 = multer({
+    storage: multerS3({
+      s3: s3,
+      bucket: 'matius-tp-test',
+      metadata: function (req, file, cb) {
+        cb(null, {fieldName: file.fieldname});
+      },
+      key: function (req, file, cb) {
+        cb(null, Date.now() + "." + mimeTypes.extension(file.mimetype))
+      }
+    })
+  })
+
+
+  const storage = multer.diskStorage({
+    destination : 'uploads/',
+    filename : function(req,file,cb){
+        cb("",Date.now() + "." + mimeTypes.extension(file.mimetype));
+    }
+  });
+  
+  const upload = multer({
+      storage : storage
+  })
+
+
+
 
 router.get('/' , async (req,res) => {
  res.send("hola mundo");
@@ -22,6 +46,9 @@ router.get('/' , async (req,res) => {
 router.post("/files",upload.single('Archivo'),(req,res)=>{
 res.send('good');
 });
+router.post("/filess3",uploads3.single('Archivo'),(req,res)=>{
+    res.send('good');
+    });
 
 
 
